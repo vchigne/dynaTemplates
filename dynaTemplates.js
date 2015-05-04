1fb9519057a95de0fadca219ba87a98a34d0aaca
Victor Chigne <vchigne@yahoo.com>
2015-05-04 04:38:12 -0500

    Update dynaTemplates.js

--- dynaTemplates.js
+++ dynaTemplates.js
@@ -1,4 +1,4 @@
-createDynaTemplate= function (tmplt,clss,dvid)
+createDynaTemplate= function (tmplt,clss,dvid) {
 /* tmplt = Template Name
    clss = Class used by the template
    dvid = ID asigned to the created DIV
@@ -8,9 +8,9 @@ createDynaTemplate= function (tmplt,clss,dvid)
     div.id = dvid;
     document.body.appendChild(div);
     var renderedTmpl = Blaze.render(Template[tmplt], div);
-}
+}:
 
 deleteDynaTemplate=function(dvid){
     var div = document.getElementById(dvid);
     div.parentNode.removeChild(div);
-}
+}:
